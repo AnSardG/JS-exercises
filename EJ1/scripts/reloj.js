1b0669d31d64d751637b188ffcec97d437a237b1
AnSardG <antoniosardgonzalez@gmail.com>
2023-10-20 15:38:10 +0200

    Add clock exercise

---
+++ EJ1/scripts/reloj.js
@@ -0,0 +1,19 @@
+window.setInterval(()=> {    
+    const reloj = document.getElementById("reloj");
+    reloj.textContent = `Hora: ${formDate()}`;
+}, 1000);
+
+function formDate(){
+    const date = new Date();
+    let fecha = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    if(date.getHours() < 10){
+        fecha = `0` + fecha;
+    }
+    if(date.getMinutes() < 10){
+        fecha = fecha.slice(0, fecha.indexOf(":") + 1) + `0` + fecha.slice(3);
+    }
+    if(date.getSeconds() < 10){
+        fecha = fecha.slice(0, fecha.lastIndexOf(":") + 1 ) + `0` + fecha.slice(-1);
+    }        
+    return fecha;
+}
