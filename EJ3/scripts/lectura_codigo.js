4d423df089ac2ed18cf644fb24709bace53fc5d5
AnSardG <antoniosardgonzalez@gmail.com>
2023-11-08 15:40:46 +0100

    Start exercises part 3

---
+++ EJ3/scripts/lectura_codigo.js
@@ -0,0 +1,88 @@
+// Fragmento 1
+console.log("Empezamos...");
+const promise1 = new Promise((resolve, reject) => {
+console.log("Orden 1");
+})
+console.log("Se acabó!!");
+
+/**
+ * Empezamos...
+ * Orden 1
+ * Se acabó!!
+ */
+
+// Fragmento 2
+console.log("Empezamos...");
+const promise2 = new Promise((resolve, reject) => {
+console.log("Orden uno")
+resolve("Orden dos")
+})
+promise2.then(respuesta => {
+console.log(respuesta)
+})
+console.log("Se acabó!!");
+
+/**
+ * Empezamos...
+ * Orden uno
+ * Se acabó!!
+ * Orden dos
+ */
+
+
+// Fragmento 3
+console.log("Empezamos...");
+const promise3 = new Promise((resolve, reject) => {
+console.log("Orden uno")
+
+resolve("Orden dos")
+console.log("Orden tres")
+})
+promise3.then(respuesta => {
+console.log(respuesta)
+})
+console.log("Se acabó!!");
+
+/**
+ * Empezamos..
+ * Orden uno
+ * Orden tres
+ * Se acabó!!
+ * Orden dos
+ */
+
+// Fragmento 4
+console.log("Empezamos...");
+const promise4 = new Promise((resolve, reject) => {
+console.log("Orden uno")
+})
+promise4.then(respuesta => {
+console.log("Orden dos")
+})
+console.log("Se acabó!!");
+
+/**
+ * Empezamos..
+ * Orden uno
+ * Se acabó!!
+ */
+
+// Fragmento 5
+console.log("Empezamos...")
+const fn = () => (new Promise((resolve, reject) => {
+console.log("Orden uno");
+resolve("Ok")
+}))
+console.log("Orden dos")
+fn().then(res => {
+console.log(res)
+})
+console.log("Se acabó!!")
+
+/**
+ * Empezamos..
+ * Orden dos
+ * Orden uno
+ * Se acabó!!
+ * Ok
+ */
