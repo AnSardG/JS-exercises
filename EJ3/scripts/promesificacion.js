59535b21d68f0fb46fda1d1430bce3c7019d284b
AnSardG <antoniosardgonzalez@gmail.com>
2023-11-09 18:39:12 +0100

    2nd exercise done

---
+++ EJ3/scripts/promesificacion.js
@@ -0,0 +1,38 @@
+/*
+
+// Función síncrona que gestiona una función callback.
+function getData(callback) {
+    if (Math.random() > 0.2) {
+        //2 segundos para ejecutar la función flecha.
+        setTimeout(() => {
+            //Se utiliza la función callback pasando por parámetro una cadena.
+            callback("Ok. Los datos han llegado");
+        }, 2000);
+    } else {
+        // Se utiliza la función callback pasando por parámetro una cadena.
+        callback("Error en la comunicación!!");
+    }
+}
+// Llamada a la función getData() con una función flecha (callback).
+getData(data => {
+    console.log(data);
+});
+
+*/
+
+const promise = new Promise((resolve, reject) => {
+    if (Math.random() > 0.9) {
+        setTimeout(() => {
+            resolve("Ok. Los datos han llegado");
+        }, 2000);
+    } else {
+        reject("Error en la comunicación!!");
+    }
+});
+
+promise.then((mensaje) => {
+    console.log(mensaje);
+})
+    .catch((error) => {
+        console.log(error);
+    });
