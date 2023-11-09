9fcfa30e8d180e86cd0c097ad1fbb71f37c9108c
AnSardG <antoniosardgonzalez@gmail.com>
2023-11-09 20:37:58 +0100

    Minor fix

--- EJ3/scripts/mapa.js
+++ EJ3/scripts/mapa.js
@@ -1,5 +1,5 @@
 async function cargarMapa() {
-    const apiKey = document.getElementById('apiKey').value;
+    const apiKey = document.getElementById("apiKey").value;
 
     if (!apiKey) {
         alert("Por favor, introduce tu API Key.");
@@ -16,7 +16,7 @@ async function cargarMapa() {
         if (response.ok) {
             const imageUrl = data.datos;
 
-            document.getElementById('mapa').innerHTML = `<img src="${imageUrl}" alt="Mapa del Día">`;
+            document.getElementById('mapa').innerHTML = `<img src="${imageUrl}" alt="Mapa del Día" style="transform:rotate(90deg); margin-left:200px;">`;
         } else {
             throw new Error(`Error: ${data.estado}`);
         }
