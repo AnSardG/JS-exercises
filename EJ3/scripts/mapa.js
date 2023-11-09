0379284725ad2433258ca660efff85f9933b129e
AnSardG <antoniosardgonzalez@gmail.com>
2023-11-09 20:25:30 +0100

    minor change

--- EJ3/scripts/mapa.js
+++ EJ3/scripts/mapa.js
@@ -1,16 +1,19 @@
 async function cargarMapa() {
-    const apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbnRvbmlvc2FyZC4yMkBjYW1wdXNjYW1hcmEuZXMiLCJqdGkiOiI5Y2Y1Nzg5YS01ODIwLTRhMWItYmU3Zi02NjVjYzUyNTJiYmMiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTY5OTU1NDcyOSwidXNlcklkIjoiOWNmNTc4OWEtNTgyMC00YTFiLWJlN2YtNjY1Y2M1MjUyYmJjIiwicm9sZSI6IiJ9.EFPqy7EUPIQxvcr8wjUkk - lz06XTMRFf2CbSkU - Avvo";
+    const apiKey = document.getElementById('apiKey').value;
+
+    if (!apiKey) {
+        alert("Por favor, introduce tu API Key.");
+        return;
+    }
 
     // URL de la API de AEMET
     const apiUrl = `https://opendata.aemet.es/opendata/api/mapasygraficos/analisis?api_key=${apiKey}`;
 
     try {
-        // Hacer la solicitud a la API
         const response = await fetch(apiUrl);
         const data = await response.json();
 
         if (response.ok) {
-            // Extrae la URL de la imagen del mapa del día
             const imageUrl = data.datos;
 
             document.getElementById('mapa').innerHTML = `<img src="${imageUrl}" alt="Mapa del Día">`;
@@ -18,12 +21,11 @@ async function cargarMapa() {
             throw new Error(`Error: ${data.estado}`);
         }
     } catch (error) {
-        // Maneja los errores de la solicitud        
+        alert("Error al cargar el mapa. Por favor, inténtalo de nuevo.");
         console.error(error.message);
     }
 }
 
-const btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
+document.getElementById("btn").addEventListener("click", () => {
     cargarMapa();
 });
