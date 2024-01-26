d04e73cbcb587ec57b0d7c4daf0f0f70485f75bc
AnSardG <antoniosardgonzalez@gmail.com>
2024-01-26 20:34:01 +0100

    Add genre, single validation and refactor code

--- 2ºTrimestre/Entregas/formulario-multipaso/js/main.js
+++ 2ºTrimestre/Entregas/formulario-multipaso/js/main.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // ENTORNO del algoritmo:
     const formulario = document.getElementById("formRegistro");
     const pasos = document.querySelectorAll(".paso");
     const botonesSiguiente = document.querySelectorAll(".btn-siguiente");
     const progressBar = document.querySelector(".progress-bar")
     const botonesAnterior = document.querySelectorAll(".btn-anterior");
     const botonesPassword = document.querySelectorAll(".btn-eye");
+    const generoInput = document.getElementById("genero");
     const passwordInput = document.getElementById("password");
     const passwordRepeatInput = document.getElementById("repeat-password");
+    const emailInput = document.getElementById("email");
+    const movilInput = document.getElementById("phone");
+    const salarioInput = document.getElementById("salario");
 
     let pasoActual = 1;
+    //Guardamos cuantos carácteres queremos como mínimo en los campos de tipo text de forma global.
     let longitudInput = 4;
 
+    //FUNCIONES:
 
     function actualizarBarraDeProgreso() {
         const progreso = ((pasoActual - 1) / (pasos.length - 1)) * 100;
@@ -26,7 +33,9 @@ document.addEventListener("DOMContentLoaded", function () {
             }
             pasos[pasoActual - 1].style.display = "block";
             actualizarBarraDeProgreso();
-        }        
+        }       
+        
+        console.log(pasoActual);
     }
 
     function pasoAnterior() {
@@ -40,73 +49,136 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function validarCampos() {
-        const camposPasoActual = pasos[pasoActual - 1].querySelectorAll('input[type="text"]');
-        const passwordsPasoActual = pasos[pasoActual - 1].querySelectorAll('input[type="password"]');
-        const emailPasoActual = pasos[pasoActual - 1].querySelector('input[type="email"]');
-        const movilPasoActual = pasos[pasoActual - 1].querySelector('input[type="tel"');
-
+        // Guardamos los campos de tipo texto del paso actual y inicializamos la variable validado como verdadera.        
+        const camposPasoActual = pasos[pasoActual - 1].querySelectorAll('input[type="text"]');        
         let validado = true;
 
+        //Recorremos todos los campos de tipo texto del paso actual.
         for (const campo of camposPasoActual) {   
 
             switch (campo.id) {
                 case "nombre":
+                    //Para el nombre queremos que no introduzcan un nombre menor de 2 carácteres.
                     if(campo.value.trim().length < 2){
-                        validado = false;
+                        validado = false;                        
+                    } else {
+                        validado = true;
                     }
+
+                    //Esta forma de validar la veremos en todos los inputs que vayamos validando, se llama
+                    //a la funcion validarCampo con el campo actual y si ha pasado la validación o no.
+                    validarCampo(campo, validado);
                     break;  
 
                 default:
                     if (campo.value.trim().length < longitudInput) {                                        
                         validado = false;
+                        validarCampo(campo, validado);
+                    } else if(!validado) {
+                        //En este caso, para que se valide solamente este campo necesitamos comprobar
+                        //el valor de la variable validado, ya que si la igualamos a verdadero
+                        //entrará al siguiente paso aunque los campos anteriores a este no estuviesen validados.
+                        validarCampo(campo, !validado);
                     }
+
                     break;
+            }
+            
+            
+        }
+        
+        if(pasoActual == 1) {
+            if(generoInput.value == '') {
+                validado = false                
+                validarCampo(generoInput, validado);
+            } else if (!validado) {
+                validarCampo(generoInput, !validado);
             }
             
         }
 
-        for(const password of passwordsPasoActual) {
+        if(pasoActual == 2){
 
-            if(password.value.trim().length < longitudInput) {                
+            if(passwordInput.value.trim().length < longitudInput) {
                 validado = false;
-            } else {
-                if(password.id == "password") {
-                    
-                } else if (password.id == "repeat-password") {
-                    
+                validarCampo(passwordInput, validado);
+            } else if(!validado){
+                validarCampo(passwordInput, !validado);
+            }
+
+            
+            
+            if(passwordRepeatInput.value.trim().length < longitudInput 
+            || passwordRepeatInput.value != passwordInput.value) {
+                validado = false;
+                validarCampo(passwordRepeatInput, validado);
+            } else if (!validado) {
+                validarCampo(passwordRepeatInput, !validado);
+            }
+
+            
+
+        }
+
+        if (pasoActual == 3) {
+            if (emailInput != null) {
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                if (!emailRegex.test(emailInput.value.trim())) {                
+                    validado = false;
+                } else {
+                    validado = true;
                 }
-            }  
-
-            if (password.id === "password" || password.id === "repeat-password") {
-
-                eyeButton.addEventListener('click', () => togglePasswordVisibility(password, eyeButton));
-            }          
-        
-        }
-
-        if (emailPasoActual != null) {
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            if (!emailRegex.test(emailPasoActual.value.trim())) {
-                // Si el email no cumple con el formato, muestra un mensaje de error o realiza la lógica que desees.
-                validado = false;
-            }
-        }
     
-        if (movilPasoActual != null) {
+                validarCampo(emailInput, validado);
+            }
+        
+            if (movilInput != null) {
+    
+                /**
+                 * Expresión regular que comprueba teléfonos españoles con cualquiera de los siguientes formatos:
+                 * +346xxxxxxxx
+                 * 00346xxxxxxxx
+                 * 346xxxxxxxx
+                 * 6xxxxxxxx
+                 * 7xxxxxxxx
+                 * 8xxxxxxxx
+                 * 9xxxxxxxx
+                 */          
 
-            //Teléfonos españoles
-            const movilRegex = /^(?:(?:\+|00)34)?[6-9]\d{8}$/;
-            if (!movilRegex.test(movilPasoActual.value.trim())) {
-                // Si el móvil no cumple con el formato, muestra un mensaje de error o realiza la lógica que desees.
-                validado = false;
+                const movilRegex = /^(?:(?:\+|00)34)?[6-9]\d{8}$/;
+                if (!movilRegex.test(movilInput.value.trim())) {
+                    validado = false;
+                    validarCampo(movilInput, validado);
+                } else if (!validado) {
+                    validarCampo(movilInput, !validado);
+                }
+                
             }
         }
-
-        if(!validado) {
-            alert('Todos los campos deben estar llenos.');
-        }
+        
+        if (pasoActual == 4) {
+            if (salarioInput != null) {
+                if(salarioInput.value < 300) {
+                    validado = false;
+                } else {
+                    validado = true;
+                    salarioPresupuesto = salarioInput.value - 172;
+                    alert("Tras los gastos de comida al mes aproximados en España, su sueldo sería: " + salarioPresupuesto + "€.");
+                }
+                
+                validarCampo(salarioInput, validado);
+            }
+        }                
 
         return validado;
+    }
+
+    function validarCampo(campo, valido){
+        if(valido){
+            campo.classList.remove('campo-invalido')
+        } else {
+            campo.classList.add('campo-invalido')
+        }
     }
 
     function togglePasswordVisibility(id) {           
@@ -131,4 +203,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     formulario.addEventListener("submit", (e) => e.preventDefault());
+    
+
+    document.addEventListener("keydown", function (e) {
+
+        // Comprobamos si se ha pulsado la tecla Enter y si es así se intentará continuar al siguiente paso.
+        if (e.key === "Enter"){
+            // Evita que el formulario se envíe por defecto
+            e.preventDefault();
+
+            siguientePaso();
+        }
+    })
 });
