b9372b285e5c6dcccd77029b9c56b0414cf48229
Antonio Sard González <107388142+AnSardG@users.noreply.github.com>
2024-02-08 18:43:56 +0000

    Done implementing CRUD logic, TODO: routing fix

--- 2ºTrimestre/Entregas/blog/src/app/app.component.ts
+++ 2ºTrimestre/Entregas/blog/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostListComponent } from './post/post-list/post-list.component';
 import { PostCreateComponent } from './post/post-create/post-create.component';
+import { PostUpdateComponent } from './post/post-update/post-update.component';
+import { PostDetailComponent } from './post/post-detail/post-detail.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PostListComponent, PostCreateComponent],
+  imports: [RouterOutlet, PostListComponent, PostCreateComponent, PostUpdateComponent, PostDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 2ºTrimestre/Entregas/blog/src/app/post/post-detail/post-detail.component.ts
+++ 2ºTrimestre/Entregas/blog/src/app/post/post-detail/post-detail.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Post } from '../post';
+import { PostService } from '../post.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.css'
 })
-export class PostDetailComponent {
+export class PostDetailComponent implements OnInit{
+  post!: Post;
 
+  constructor(private route: ActivatedRoute, private postService: PostService) { }
+
+  ngOnInit(): void {
+    const postId = +!this.route.snapshot.paramMap.get('id');
+    this.postService.find(postId).subscribe((post: Post) => {
+      this.post = post;
+    });
+  }
 }
