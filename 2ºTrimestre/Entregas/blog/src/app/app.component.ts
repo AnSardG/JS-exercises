1519af07d078290280889d036cbba5b1c4d2cfe6
Antonio Sard González <107388142+AnSardG@users.noreply.github.com>
2024-02-08 18:21:45 +0000

    Start CRUD components

--- 2ºTrimestre/Entregas/blog/src/app/app.component.ts
+++ 2ºTrimestre/Entregas/blog/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { PostCreateComponent } from './post/post-create/post-create.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PostListComponent, PostCreateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- 2ºTrimestre/Entregas/blog/src/app/app.routes.ts
+++ 2ºTrimestre/Entregas/blog/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { PostDetailComponent } from './post/post-detail/post-detail.component';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+import { PostUpdateComponent } from './post/post-update/post-update.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'posts', component: PostListComponent }, 
+    { path: 'posts/:id', component: PostDetailComponent },
+    { path: 'create', component: PostCreateComponent },
+    { path: 'update/:id', component: PostUpdateComponent } 
+];
